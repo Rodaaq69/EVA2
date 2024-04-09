a6b1302b3cfb992b2c0d971a9bdf2185fbd1d054
Rodaaq69 <122701094+Rodaaq69@users.noreply.github.com>
2024-04-09 16:52:22 -0600

    Add files via upload

---
+++ Repositorio EVA2/EVA2_18_FOR/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i=1; i<=10; i++) {
+            System.out.print(i+"-");
+        }
+        for(int i=10; i>=1; i--) {
+            System.out.print(i+"-");
+        }
+
+        for(int i=0; i<=100; i+=2) {
+            System.out.println(i+"-");
+        }
+
+
+    }
+}
