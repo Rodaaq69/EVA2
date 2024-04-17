1f5bad37febd0ff5b48b118e1bbfde42d87f2405
Rodaaq69 <122701094+Rodaaq69@users.noreply.github.com>
2024-04-17 16:53:25 -0600

    Add files via upload

---
+++ Repositorio EVA2/EVA2_25/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+        for(int i=0; i<100; i++ ){
+            int mod=1%2;
+            if(mod!=0);
+           
+            continue;//Cuando se ejecuta interrumpe la iteración
+
+        }
+    }
+}
---
+++ Repositorio EVA2/EVA2_26/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String usuario, pwd;
+
+        String USUARIO = "Admin";
+        String PASSWORD="Admin";
+        Scanner read = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Usuario:");
+            usuario= read.nextLine();
+            System.out.println("Password");
+            pwd=read.nextLine();
+            if(usuario.equals(USUARIO)&&pwd.equals(PASSWORD)){
+                break;
+            }
+            System.out.println("Bienvenido al sistema");
+            
+
+        }
+            
+    }
+}
